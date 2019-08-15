e3584d8cc0e78a537bfb9f40b305133bc6d5a209
pahosler <pahosler@gmail.com>
2019-08-15 04:05:52 -0500

    registered gallery preview cms

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,7 +1,9 @@
 import CMS from 'netlify-cms-app'
 import '../assets/sass/styles.sass'
 import HomePagePreview from './preview-templates/HomePagePreview'
+import GalleryPagePreview from './preview-templates/GalleryPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
 
 CMS.registerPreviewTemplate('home', HomePagePreview)
+CMS.registerPreviewTemplate('gallery', GalleryPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
